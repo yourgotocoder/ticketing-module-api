387c050f6df3862774f062c943146d22012e175e
yourgotocoder <yourgotocoder@gmail.com>
2022-04-05 10:22:14 +0530

    First version of the connector function implemented

--- db/connector.js
+++ db/connector.js
@@ -1,1 +1,18 @@
 //A connector function for mongodb
+const mongoose = require("mongoose");
+
+async function mongoDBConnector(dbUserName, dbPassword, callbackFn) {
+    try {
+        const result = await mongoose.connect(
+            `mongodb+srv://${dbUserName}:${dbPassword}@mongodb-graphql.qg7ft.mongodb.net/ticketing-module?retryWrites=true&w=majority`
+        );
+        result
+            ? console.log("Connected successfully to mongo database")
+            : new Error(`Error connecting to database`);
+        callbackFn();
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+module.exports = mongoDBConnector;
--- index.js
+++ index.js
@@ -2,10 +2,24 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+//Custom imports
+const mongoConnector = require("./db/connector");
+
 const app = express();
+
+//Middlewares
+app.use(cors());
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, () =>
-    console.log(`REST server for Ticketing Module started on port: ${PORT}`)
+mongoConnector(
+    process.env.MONGO_DB_USER_NAME,
+    process.env.MONGO_DB_PASSWORD,
+    () => {
+        app.listen(PORT, () =>
+            console.log(
+                `REST server for Ticketing Module started on port: ${PORT}`
+            )
+        );
+    }
 );
